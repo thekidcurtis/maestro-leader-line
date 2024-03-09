ed2a3e6f9a7478a96f786812bfcf9cef758a97ab
thekidcurtis <kid.curtis@gmail.com>
2024-03-09 09:59:13 -0500

    Update function-test.js

--- test/function-test/function-test.js
+++ test/function-test/function-test.js
@@ -1,11 +1,5 @@
 /* exported functionTest */
 
-// Add new leader line from `start` to `end` (HTML/SVG element, basically).
-new LeaderLine(
-  document.getElementById('start'),
-  document.getElementById('end')
-);
-
 var functionTest = (function() {
   'use strict';
 
